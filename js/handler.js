ff76a349bb9e1e872b8ee1da2ce0db15ba636d01
piginzoo <piginzoo@gmail.com>
2019-05-20 21:31:21 +0800

    update

--- js/handler.js
+++ js/handler.js
@@ -1,32 +1,39 @@
-console.log("自动，实时监控ing");  
+console.log("自动报名，实时监控ing");  
 $(document).ready(function(){ 
 
-    reloadit()
+    setTimeout('detect()',30000)
 
 });
 
-function reloadit(){
-    console.log("刷新页面................")
-    setTimeout('detect()', 3000);
-}
-
 
 function detect(){
+
     
     var all = $('li.clearfix.ng-scope')
 
+    var is_have_lessons = false
     all.each(function(){
         val = $(this).find('.postion_abt p.ng-binding').text()
         title= $(this).find('.postion_rel.fc33.ng-binding').text()
         if (val!="0"){
             console.log("我靠！有课了！")
             notifyMe(title)
+            is_have_lessons = true
         }else{
+            //$(this).find("a.postion_rel.fc33.ng-binding").trigger("click");
+            //console.log($(this).find("a.postion_rel.fc33.ng-binding"))
             console.log(title+",没动静...")
         }
     })
 
-    setTimeout('reloadit()', 7000);
+    if(!is_have_lessons){
+        console.log("刷新页面................")
+        window.location.reload()
+    }
+}
+
+function notifyMe(title) {
+    document.location="mailto:piginzoo@qq.com;?subject=快快去抢《"+title+"》的课"
 }
 
 /**
